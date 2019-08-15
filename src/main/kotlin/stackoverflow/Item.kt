2c1a06fef270dd9749c453a18fa41b2f0093333c
Bastien Müllers <bastien.mullers@student.unamur.be>
2019-08-15 22:44:00 +0200

    add response body decompression and object serialization

---
+++ src/main/kotlin/stackoverflow/Item.kt
@@ -0,0 +1,30 @@
+package stackoverflow
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+class Item {
+
+    @SerializedName("owner")
+    @Expose
+    var owner: Owner? = null
+    @SerializedName("is_accepted")
+    @Expose
+    var isAccepted: Boolean? = null
+    @SerializedName("score")
+    @Expose
+    var score: Int? = null
+    @SerializedName("last_activity_date")
+    @Expose
+    var lastActivityDate: Int? = null
+    @SerializedName("creation_date")
+    @Expose
+    var creationDate: Int? = null
+    @SerializedName("answer_id")
+    @Expose
+    var answerId: Int? = null
+    @SerializedName("question_id")
+    @Expose
+    var questionId: Int? = null
+
+}
--- src/main/kotlin/stackoverflow/stackOverflowQuery.kt
+++ src/main/kotlin/stackoverflow/stackOverflowQuery.kt
@@ -1,16 +1,23 @@
 package stackoverflow
 
 import Logger
-
+import com.google.gson.Gson
+import java.io.BufferedReader
+import java.io.ByteArrayInputStream
+import java.io.InputStreamReader
 import java.time.LocalDateTime
 import java.time.ZoneOffset
+import java.util.zip.GZIPInputStream
 import javax.ws.rs.client.Client
 import javax.ws.rs.client.ClientBuilder
 import javax.ws.rs.core.MediaType
 
+
 lateinit var client : Client
 
 val LOG = Logger("StackOverflow Queries")
+val gson = Gson()
+
 
 fun restClientInit(){
     client = ClientBuilder.newClient()
@@ -27,5 +34,21 @@ fun makeQuery(){
         .queryParam("order","desc") //Descending
         .queryParam("sort","votes") //On amount of votes
         .queryParam("site","stackoverflow") //From StackOverflow
-    println(target.request(MediaType.APPLICATION_JSON).get())
+    val builder = target.request(MediaType.TEXT_PLAIN_TYPE).header("Accept-Encoding","GZIP");
+    var response = builder.get()
+    val gzipInputStream = GZIPInputStream(ByteArrayInputStream(response.readEntity(ByteArray::class.java)))
+    val reader = InputStreamReader(gzipInputStream)
+    val bufferedReader = BufferedReader(reader)
+    var inflated = ""
+    var read = bufferedReader.readLine()
+    while(read != null){
+        inflated += read
+        println(read)
+        read = bufferedReader.readLine()
+    }
+    if(inflated != ""){
+        val responseBody = gson.fromJson(inflated,Response::class.java)
+        println(responseBody)
+    }
+    else LOG.error("Stack App API request returned nothing")
 }
